8a72fa51d81357a116946ae1bcc854e2e1482de2
shiyaox <shiyaox@student.chalmers.se>
2023-09-08 10:21:31 +0200

    Initial Author Routes

--- server.js
+++ server.js
@@ -5,14 +5,19 @@ if (process.env.NODE_ENV !== 'production') {
 const express = require('express')
 const app = express()
 const expressLayouts = require('express-ejs-layouts')
+const bodyParser = require('body-parser')
+
 const indexRouter = require('./routes/index')
+const authorRouter = require('./routes/authors')
 
 app.set('view engine', 'ejs')
 app.set('views', __dirname + '/views')
 app.set('layout', 'layouts/layout')
 app.use(expressLayouts)
+app.use(bodyParser.urlencoded({ limit: '10mb', extended: false }))
 app.use(express.static('public'))
 app.use('/', indexRouter)
+app.use('/authors', authorRouter)
 
 const mongoose = require('mongoose')
 mongoose.connect(process.env.DATABASE_URL)
